e113412f80bc8ed26fe4fa95b3b557282b684292
Uberpoch <nathan.poch@uberflip.com>
2021-07-26 15:08:05 -0400

    updated

--- index.js
+++ index.js
@@ -4,15 +4,10 @@ const fs = require('fs');
 
 const clientID = '';
 const secretKey = '';
-// let tokenType = '';
 
-// const desinationStream = 7106516;
-const destHub = 118754;
-const outputFile = 'unqork';
+const outputFile = '';
 
-const ogData = require('./source.js');
-const totalItems = ogData.length;
-
+const array = [];
 
 
 const auth = async (key, secret) => {
@@ -34,48 +29,71 @@ const auth = async (key, secret) => {
 }
 
 
-const updateStream = async (token, item) => {
-    
-    return axios.get(`https://v2.api.uberflip.com/items/${item.itemId}`,
-    {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "User-Agent": "NP Script",
-            "Content-Type": "application/json",
-        }
-    })
-    .then(res => {
-        const data = res.data;
-        const formatted = {
-            stream: data.id
-        };
-        return formatted;
-    })
-    .catch(err => {
-        console.log(err.response);
-        console.log(`error in updating a stream`);
-    })
-};
+const callLoop = async function(token, array){
+    let returnedItems = [];
+    let totalItems = array.length;
+        
+    async function call(url, token){
+    axios.get(url, {
+            headers: { 
+                'Authorization': `Bearer ${token}`,
+                'User_Agent': `Nathan UF`
+              }
+        })
+        .catch(error => {
+            console.log(error);
+            })
+        .then(async (res) => {
+            let objs = res.data;
+            
+            returnedItems.push(objs);
 
-const makeLoop = async (token, array) => {
-    let runItems = 0;
-    array.forEach(async (stream) => {
-        const value = await updateStream(token, stream);
-        runItems += 1;
-        console.log(`item: ${value.stream} updated ${runItems} of ${totalItems}`);
-    })
+                if(totalItems === returnedItems.length){
+                    console.log(`returnedItems: ${returnedItems.length} = totalItems: ${totalItems}`);
+                    console.log(`creating file`);
+                    setTimeout(function(){
+                        generateFile(returnedItems)
+                    },
+                        5000
+                    );
+                    // generateFile(returnedItems)
+                }
+            
+        });  
 
+    }
+    for(let i = 0; i < array.length; i++){
+        let url = `https://v2.api.uberflip.com/items/${array[i]}`;
+        call(url, token)
+   }
 }
+
+const  generateFile = async(res) => {
+    const allowArray = [
+        "hub_id",
+        "title",
+        "description",
+        // "url",
+        "seo_title",
+        "seo_description",
+        "thumbnail_url",
+        "content",
+        // "stream_id"
+        "author"
+    ];
+    let data = JSON.stringify(res, null, 2);
+    fs.writeFileSync(`${outputFile}.json`, data);
+    console.log('json created');
+  };
 
 const run = async function(){
     const token = await auth(clientID, secretKey);
     // console.log(token);
     console.log('token created');
-    const data = await makeLoop(token,ogData);
-    console.log('complete');
-
+    const data = await callLoop(token, array);
+    // console.log(data.length);
+    console.log('data confirmed');
+    // await generateFile(data);
 
 };
 run();
-
-
